c67c829860a5ffb01fa8b4949100baa0bec2cc73
Ashassins <ashenassassinmc@gmail.com>
2022-12-19 14:00:57 -0600

    add initial XCode Project

---
+++ AiOAlarm/AiOAlarm/AiOAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiOAlarmApp.swift
+//  AiOAlarm
+//
+//  Created by Anna Shen on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AiOAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiOAlarm/AiOAlarm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AiOAlarm
+//
+//  Created by Anna Shen on 12/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
